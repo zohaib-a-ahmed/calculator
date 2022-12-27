30133a071d6439bbd60cfa6de6d5859ae2607293
zahmed <zohaib0718@gmail.com>
2022-12-26 18:04:46 -0600

    Number Pad + Result Pane

--- calculator.js
+++ calculator.js
@@ -2,6 +2,24 @@
  * Javascript file for provided caluclator creation and arithmetic.
  */
 
-// Button System Creation
+// result pane
+var result = ``
 
+//Number Pad
+const numberPad = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero'];
 
+numberPad.forEach(number => {
+  const element = document.querySelector(`#${number}`);
+  element.addEventListener(`click`, () => {
+    result += element.innerText;
+    console.log(result)
+  });
+});
+
+// Clear Function
+const clear = document.querySelector(`#AC`)
+clear.addEventListener(`click`, () => {
+    result = ``
+});
+
+//
